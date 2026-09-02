69c8ac5ae8f94ef04d1afe57609961cc6dee6cc1
john Edwards <johnteedushiks247@gmail.com>
2026-09-03 00:15:51 +0300

    Configure Vercel deployment settings

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-uf!1f2+#*h7yr8+(w184re_=ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
